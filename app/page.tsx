382a8136ab51d34fef00ff510aaa33754e06f4a5
rocketmanh <will@collective.xyz>
2023-10-12 18:18:52 -0500

    first

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      First of forever
+      First of forever ⌐◨-◨
     </main>
   )
 }
